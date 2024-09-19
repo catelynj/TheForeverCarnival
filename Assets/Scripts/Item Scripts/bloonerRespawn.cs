60089359402fd802c49b175b526946bb022a3ca3
Joe Hartland <jbh21@pct.edu>
2024-09-19 09:54:39 -0400

    Bloon respawn & button
    Still need to implement random spawn locations and a timer to stop players from spamming it

--- Assets/Scripts/Item Scripts/bloonerRespawn.cs
+++ Assets/Scripts/Item Scripts/bloonerRespawn.cs
@@ -3,11 +3,22 @@
 //Purpose: Let the player respawn the balloons by pressing 'E' on a button
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class bloonerRespawn : MonoBehaviour
 {
+
+    public int balloons;
+    public GameObject balloon;
     bool canButton = true;
+    public GameObject spawnArea;
+
+    private void Start()
+    {
+        balloonSpawner(); // Call the spawning function when the game begins
+        //blooner other = spawnArea.GetComponent<blooner>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -29,8 +40,27 @@ public class bloonerRespawn : MonoBehaviour
         {
             if (hit.collider.CompareTag("dartButton"))
             {
-                //blooner.balloonSpawner(); //Need to find a way to implement this
+                balloonSpawner();
+                //blooner blnr = new blooner();
+                //blnr.balloonSpawner(); //Need to find a way to implement this
             }
         }
     }
+
+    void balloonSpawner()
+    {
+        Collider spawn = spawnArea.GetComponent<BoxCollider>();
+        Rigidbody rigi = balloon.GetComponent<Rigidbody>();
+        for (int i = 0; balloons > i; i++)
+        {
+            //TODO: SPAWN BALLOONS IN RANDOM AREAS ALONG THE BOX COLLIDER
+            Instantiate(balloon, spawn.transform);
+
+            // BUG:
+            // When spawning more than one balloon, they can spawn inside of each other and eject out into space
+            rigi.constraints = RigidbodyConstraints.FreezePosition; // 
+            rigi.constraints = RigidbodyConstraints.FreezeRotation; // 
+        }
+    }
+
 }
