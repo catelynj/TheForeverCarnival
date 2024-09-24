0a2173d9ceabbec372f3a91c1f0b9a4dc69cee30
Joe Hartland <jbh21@pct.edu>
2024-09-24 10:43:44 -0400

    Work on cup respawns

---
+++ Assets/Scripts/Item Scripts/cupsRespawn.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cupsRespawn : MonoBehaviour
+{
+    public GameObject cups;
+    bool canButton = true;
+    public GameObject spawnArea;
+    IEnumerator cupRespawner;
+    public float waitTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CupsSpawner());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && canButton)
+        {
+            respawnCups();
+        }
+    }
+
+    void respawnCups()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit) && hit.collider.CompareTag("dartButton"))
+        {
+            Debug.Log("Button pressed!");
+            StartCoroutine(CupsSpawner());
+        }
+    }
+
+    IEnumerator CupsSpawner()
+    {
+        canButton = false;
+        Collider spawn = spawnArea.GetComponent<BoxCollider>();
+        Rigidbody rigi = cups.GetComponent<Rigidbody>();
+
+        //TODO: CREATE THE CUPS PREFAB & IMPLEMENT SPAWN LOCATION ON THE X,Y,Z OF AN EMPTY GAMEOBJECT
+        Vector3 spawnLocation = new Vector3(spawnArea.transform.position.x, spawnArea.transform.position.y, spawnArea.transform.position.z);
+        Instantiate(cups, spawnLocation, Quaternion.identity);
+
+        Debug.Log("Waiting..." + waitTime);
+        yield return new WaitForSeconds(waitTime);
+        Debug.Log("Ready!" + waitTime);
+        canButton = true;
+    }
+
+}
