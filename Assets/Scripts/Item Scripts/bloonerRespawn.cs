8aa293c80e8c9c3d4863e6aa951539a188580352
Joe Hartland <jbh21@pct.edu>
2024-09-24 10:10:00 -0400

    Blooner

--- Assets/Scripts/Item Scripts/bloonerRespawn.cs
+++ Assets/Scripts/Item Scripts/bloonerRespawn.cs
@@ -18,20 +18,17 @@ public class bloonerRespawn : MonoBehaviour
 
     private void Start()
     {
-        balloonRespawner = BalloonSpawner();
-        StartCoroutine(balloonRespawner);
+        //balloonRespawner = BalloonSpawner();
+        StartCoroutine(BalloonSpawner());
         // Call the spawning function when the game begins
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && canButton)
         {
-            if (canButton)
-            {
                 respawnBloons();
-            }
         }
     }
 
@@ -39,38 +36,34 @@ public class bloonerRespawn : MonoBehaviour
     {
         Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
+        if (Physics.Raycast(ray, out hit) && hit.collider.CompareTag("dartButton"))
         {
-            if (hit.collider.CompareTag("dartButton"))
-            {
                 Debug.Log("Button pressed!");
-                canButton = false;
-                StartCoroutine(balloonRespawner);
-                canButton = true;
-                Debug.Log("Button ready!");
-            }
+                StartCoroutine(BalloonSpawner());
         }
     }
 
     // BUG: Coroutine is only being called once and then skipped over. Not sure why.
     IEnumerator BalloonSpawner()
     {
+        canButton = false;
         Collider spawn = spawnArea.GetComponent<BoxCollider>();
         Rigidbody rigi = balloon.GetComponent<Rigidbody>();
-        for (int i = 0; balloons > i; i++)
+        for (int i = 0; i < balloons; i++)
         {
-            //TODO: SPAWN BALLOONS IN RANDOM AREAS ALONG THE BOX COLLIDER
-            Instantiate(balloon, spawn.transform);
+            // Generate a random position within the BoxCollider bounds
+            Vector3 randomPosition = new Vector3(
+                Random.Range(spawn.bounds.min.x, spawn.bounds.max.x),
+                Random.Range(spawn.bounds.min.y, spawn.bounds.max.y),
+                Random.Range(spawn.bounds.min.z, spawn.bounds.max.z)
+            );
 
-            // BUG:
-            // When spawning more than one balloon, they can spawn inside of each other and eject out into space
-            rigi.constraints = RigidbodyConstraints.FreezePosition; // 
-            rigi.constraints = RigidbodyConstraints.FreezeRotation; // 
-            
+            Instantiate(balloon, randomPosition, Quaternion.identity); // Spawn balloon at random position
         }
         Debug.Log("Waiting..." + waitTime);
         yield return new WaitForSeconds(waitTime);
         Debug.Log("Ready!" + waitTime);
+        canButton = true;
     }
 
 }
