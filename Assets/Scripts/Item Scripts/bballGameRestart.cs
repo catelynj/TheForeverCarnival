448402b274452f5ba2891b493ac71985783d715b
Joe Hartland <jbh21@pct.edu>
2024-10-10 09:36:18 -0400

    ball hoop ball

--- Assets/Scripts/Item Scripts/bballGameRestart.cs
+++ Assets/Scripts/Item Scripts/bballGameRestart.cs
@@ -4,5 +4,37 @@ using UnityEngine;
 
 public class bballGameRestart : MonoBehaviour
 {
+    public GameObject gate;
+    bool canButton = true;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && canButton)
+        {
+            startBasketballGame();
+        }
+    }
+
+    void startBasketballGame()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit) && hit.collider.CompareTag("ballButton"))
+        {
+            //Debug.Log("Ball button pressed!");
+            OpenGate();
+            StartCoroutine(BballTimer());
+        }
+    }
+
+    IEnumerator BballTimer()
+    {
+        return null;
+    }
+
+    void OpenGate()
+    {
+        gate.transform.Rotate(-90,0,0,Space.Self);
+    }
 
 }
