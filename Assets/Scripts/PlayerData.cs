0ffa14e132759c4d231cbc7a82fcec3191473121
Joe Hartland <jbh21@pct.edu>
2024-10-15 10:49:03 -0400

    Beginning of savedata functionality

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData
+{
+ /*   public int points;
+    public Vector3 playerLocation;
+    public PlayerData(Player player)
+    {
+        playerLocation = player.location;
+        points = player.points;
+    }*/
+    // https://weeklyhow.com/how-to-save-load-game-in-unity/#Creating_the_Class I'm trying this thing
+}
