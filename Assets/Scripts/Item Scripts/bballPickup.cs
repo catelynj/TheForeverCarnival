06077d81c71bdd305b7db19dc3be41c9ada34fb3
Joe Hartland <jbh21@pct.edu>
2024-10-15 10:10:57 -0400

    Working ball pickup & throw

--- Assets/Scripts/Item Scripts/bballPickup.cs
+++ Assets/Scripts/Item Scripts/bballPickup.cs
@@ -9,10 +9,10 @@ public class bballPickup : MonoBehaviour
     public float throwForce = 10f;
     public float cloneOffset = 2f;
     public float eyeLevelHeight = 1.5f;
-    private GameObject clone;
     bool beingCarried = false;
     private bool canPickup = true;
     GameObject player;
+    GameObject bball;
 
     // Start is called before the first frame update
     void Start()
@@ -36,44 +36,31 @@ public class bballPickup : MonoBehaviour
         {
             Throw();
         }
-
     }
 
     void Pickup()
     {
         Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
         RaycastHit hit;
-        Vector3 vector3 = new Vector3(0f,-10f,0f);
-        if (Physics.Raycast(ray, out hit) && hit.collider.CompareTag("basketball"))// && GameObject.Find("basketball(Clone)") == null)
+        if (Physics.Raycast(ray, out hit) && hit.collider.CompareTag("basketball"))
         {
-            clone = Instantiate(hit.collider.gameObject);
-            hit.collider.gameObject.transform.position = vector3;
-            Rigidbody rb = clone.GetComponent<Rigidbody>();
-            if (rb == null)
-            {
-                rb = clone.AddComponent<Rigidbody>();
-            }
-
-            rb.useGravity = true;
             beingCarried = true;
-            rb.constraints = RigidbodyConstraints.None;
-
+            bball = hit.collider.gameObject;
         }
-
     }
 
     void Throw()
     {
         Camera playerCamera = cam.GetComponent<Camera>();
         Vector3 throwPosition = playerCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, playerCamera.nearClipPlane + cloneOffset));
-        clone.transform.position = throwPosition;
+        bball.transform.position = throwPosition;
         Ray ray = playerCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
         Vector3 throwDirection = ray.direction;
-        clone.transform.LookAt(clone.transform.position + throwDirection);
+        bball.transform.LookAt(bball.transform.position + throwDirection);
         if (Input.GetMouseButtonDown(0))
         {
-            clone.transform.parent = null;
-            Rigidbody rb = clone.GetComponent<Rigidbody>();
+            bball.transform.parent = null;
+            Rigidbody rb = bball.GetComponent<Rigidbody>();
             if (rb != null)
             {
                 rb.velocity = throwDirection * throwForce;
