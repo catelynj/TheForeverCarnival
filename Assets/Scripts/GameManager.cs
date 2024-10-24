e332daa6b76a13cbf1378c1ffb729e00213947dc
catelynj <113139640+catelynj@users.noreply.github.com>
2024-10-24 10:54:43 -0400

    Trophy Spin and More Trophy Stuff Added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,6 +16,7 @@ public class GameManager : MonoBehaviour
     private AudioSource backgroundSource;
     public AudioClip backgroundSound;
 
+
     public static GameManager Instance
     {
         get
